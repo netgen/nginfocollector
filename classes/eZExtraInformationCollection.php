0ec08d2972f07359e799b25154cc5ae824723437
Mario Blažek <blazek.mario@gmail.com>
2018-06-05 12:53:03 +0200

    Added docblocks

--- classes/eZExtraInformationCollection.php
+++ classes/eZExtraInformationCollection.php
@@ -2,6 +2,11 @@
 
 class eZExtraInformationCollection extends \eZInformationCollection
 {
+    /**
+     * Anonymizes whole collection
+     *
+     * @param int $collectionID
+     */
     static function anonymizeCollection( $collectionID )
     {
         if( !is_numeric( $collectionID ) )
@@ -11,14 +16,21 @@ class eZExtraInformationCollection extends \eZInformationCollection
 
         $db = eZDB::instance();
 
-        $db->query("UPDATE ezinfocollection_attribute
-                    SET data_float = 0,
-                    data_int = 0,
-                    data_text = 'xxxxxxxxxx'
-                    WHERE informationcollection_id = '$collectionID'"
+        $db->query(
+        "UPDATE ezinfocollection_attribute
+            SET data_float = 0,
+            data_int = 0,
+            data_text = 'xxxxxxxxxx'
+            WHERE informationcollection_id = " . (int)$collectionID
         );
     }
 
+    /**
+     * Anonymizes given fields for collection
+     *
+     * @param int $collectionID
+     * @param array $fieldIDs
+     */
     static function anonymizeFieldForCollection( $collectionID, array $fieldIDs )
     {
         if( !is_numeric( $collectionID ) )
@@ -35,15 +47,22 @@ class eZExtraInformationCollection extends \eZInformationCollection
 
         $db = eZDB::instance();
 
-        $db->query("UPDATE ezinfocollection_attribute
-                SET data_float = 0,
-                data_int = 0,
-                data_text = 'xxxxxxxxxx'
-                WHERE informationcollection_id = '$collectionID'
-                AND contentobject_attribute_id IN ($fieldIDs)"
+        $db->query(
+            "UPDATE ezinfocollection_attribute
+            SET data_float = 0,
+            data_int = 0,
+            data_text = 'xxxxxxxxxx'
+            WHERE informationcollection_id = " . (int)$collectionID
+            . " AND contentobject_attribute_id IN ($fieldIDs)"
         );
     }
 
+    /**
+     * Removes fields from collection
+     *
+     * @param int $collectionID
+     * @param array $fieldIDs
+     */
     static function deleteAttributesForCollection( $collectionID, array $fieldIDs )
     {
         if( !is_numeric( $collectionID ) )
@@ -60,55 +79,72 @@ class eZExtraInformationCollection extends \eZInformationCollection
 
         $db = eZDB::instance();
 
-
-        $db->query("DELETE FROM ezinfocollection_attribute
-                WHERE informationcollection_id = '$collectionID'
-                AND contentobject_attribute_id IN ($fieldIDs)"
+        $db->query(
+            "DELETE FROM ezinfocollection_attribute
+            WHERE informationcollection_id = " . (int)$collectionID
+            . " AND contentobject_attribute_id IN ($fieldIDs)"
         );
     }
 
-    static function fetchCollectionsBySearchTest($contentObjectID, $searchText, $offset = 0, $limit = null)
+    /**
+     * Performs search withing collection attributes
+     *
+     * @param int $contentObjectID
+     * @param string $searchText
+     * @param int $offset
+     * @param int $limit
+     *
+     * @return array
+     */
+    static function fetchCollectionsBySearchTest($contentObjectID, $searchText, $offset = 0, $limit = 10)
     {
         $objects = [];
 
         $db = eZDB::instance();
 
-        $searchText = strtolower($searchText);
+        $results = $db->query(
+        "SELECT id
+            FROM ezinfocollection
+            WHERE id in (SELECT informationcollection_id as id
+                         FROM ezinfocollection_attribute
+                         WHERE LOWER(data_text) like '%" . $db->escapeString(strtolower($searchText)) . "%')
+            AND contentobject_id = " . (int)$contentObjectID
+            . " LIMIT " . (int)$offset . "," . (int)$limit
+        );
 
-        $queryString = "SELECT id
-                        FROM ezinfocollection
-                        WHERE id in (SELECT informationcollection_id as id
-			                         FROM ezinfocollection_attribute
-			                         WHERE LOWER(data_text) like '%$searchText%')
-                        AND contentobject_id = $contentObjectID
-                        LIMIT $offset, $limit";
-
-        $results = $db->query($queryString);
-
-        foreach ($results->fetch_all() as $result) {
-            $objects[] = eZInformationCollection::fetch($result[0]);
+        foreach ($results as $result) {
+            $objects[] = eZInformationCollection::fetch($result['id']);
         }
 
         return $objects;
     }
 
+    /**
+     * Returns count for search
+     *
+     * @param int $contentObjectID
+     * @param string $searchText
+     *
+     * @return int
+     */
     static function fetchCollectionsCountBySearchTest($contentObjectID, $searchText)
     {
         $db = eZDB::instance();
 
-        $searchText = strtolower($searchText);
+        $results = $db->arrayQuery(
+            "SELECT COUNT(id) as count
+            FROM ezinfocollection
+            WHERE id in (SELECT informationcollection_id as id
+                         FROM ezinfocollection_attribute
+                         WHERE LOWER(data_text) like '%" . $db->escapeString(strtolower($searchText)) . "%')
+            AND contentobject_id = " . (int)$contentObjectID,
+            array(
+                "column" => array(
+                    "count"
+                ),
+            )
+        );
 
-        $queryString = "SELECT COUNT(id) as count
-                        FROM ezinfocollection
-                        WHERE id in (SELECT informationcollection_id as id
-			                         FROM ezinfocollection_attribute
-			                         WHERE LOWER(data_text) like '%$searchText%')
-                        AND contentobject_id = $contentObjectID";
-
-        $results = $db->query($queryString);
-
-        $result = $results->fetch_assoc();
-
-        return $result['count'];
+        return (int)$results[0]['count'];
     }
 }
